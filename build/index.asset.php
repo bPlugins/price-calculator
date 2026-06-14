690a3b19cdea8e2345c248b033d57c04b084a8ce
wasek23 <wasekbellah2311@gmail.com>
2026-06-14 10:08:02 +0600

    update v 1.1.1

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '556e84ffeab9c078e33d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '4d6eebaceb2e154ff3d3');
